51ebd77c3f592152554d78e29f88fff5eabd641a
Gikkman <ksr.jonasson@gmail.com>
2017-03-20 22:46:28 +0100

    updates

--- modules/user_database.js
+++ modules/user_database.js
@@ -8,9 +8,9 @@ module.exports = function(MySQL){
 		user.display_name = data.display_name;
 		user.verified = data.email ? true : false;
 
-		MySQL.query('INSERT INTO users (created, user_id, display_name, verified) VALUES(CURRENT_TIMESTAMP,?,?,?) ' +
-				    'ON DUPLICATE KEY UPDATE verified=?, display_name=?', 
-			[user._id, user.display_name, user.verified, user.verified, user.display_name],
+		MySQL.query('INSERT INTO users (created, last_seen, user_id, user_name, display_name, verified) VALUES(CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,?,?,?,?) ' +
+				    'ON DUPLICATE KEY UPDATE last_seen=CURRENT_TIMESTAMP, verified=?, display_name=?', 
+			[user._id, user.name, user.display_name, user.verified, user.verified, user.display_name],
 		    (err, rows) => {
 		    	if( err )
 		    		onError(err);
