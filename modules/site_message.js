8cdf5261f20a0e479615e28c6dceea53aab1bb62
Gikkman <ksr.jonasson@gmail.com>
2018-05-26 23:47:09 +0200

    added a couple of new site messages

--- modules/site_message.js
+++ modules/site_message.js
@@ -13,15 +13,16 @@ module.exports = () => {
             message:
                 "<p>"
               + "This is your first time here, right? Allow me to provide you"
-              + " with a brief introduction."
+              + " with a brief introduction o how to submit a quest to the "
+              + " Journey Project."
               + "<p>"
-              + "Before making your first submission, please take your time to"
-              + " check our " + SAFE_SYSTEMS + ", to make sure you do not make a"
-              + " submission that we can't play. Also, if you wanna see what"
+              + "Before making your first quest submission, please take your time to"
+              + " check our " + SAFE_SYSTEMS + ", to make sure you do not submit a"
+              + " quest that we can't play. Also, if you wanna see what"
               + " we've played earlier, check out our " + QUEST_LOG + "."
               + "<p>"
-              + "Once you've made a submission, it will stay in our raffle pool"
-              + " until you win or manually delete it. Once your submission's"
+              + "Once you've made a quest submission, it will stay in our raffle pool"
+              + " until you win or manually delete it. Once your quest's"
               + " been played, if it wasn't completed, you will have the ability"
               + " to resubmit it (Gikk might also re-submit it himself). If"
               + " it was completed, or you don't want to resubmit it, you may "
@@ -29,6 +30,72 @@ module.exports = () => {
               + "<p>"
               + "Happy submitting, and if there is anything you're wondering,"
               + " just ask at our " + STREAM + "."
+        },
+        COMPLETED: {
+            title: "Quest completed!",
+            message:
+                "<p>"
+              + "Your quest was completed on {date}, after a playtime of"
+              + " {time} (total playtime {total_time}, played {playtimes} times)!"
+              + "<p>"
+              + "Gikkman gave it a rating of '{rating}'. If you want to check"
+              + " out the entire review, please check out the " + QUEST_LOG + "."
+              + "<p>"
+              + "All that is left to do now is to confirm the completion, and"
+              + " then you are all set up make a new quest submission. Thanks for"
+              + " contributing to the Journey Project!"
+        },
+        SUSPENDED: {
+            title: "Quest suspended",
+            message:
+                "<p>"
+             + "Your quest was suspended on {date}. Gikkman couldn't get it"
+             + " up and running in a timely fashion, and chose to suspend it"
+             + " and do some research. He will do his uttermost to get the"
+             + " game running as soon as he can. "
+             + "<p>"
+             +"Gikkman left the following message to you:<br>"
+             + "'{comment}'"
+             + "<p>"
+             + "Suspended games are left hanging in limbo until Gikkman"
+             + " manages to get them to work. In case you don't have the"
+             + " patience, you can opt to abandon the quest and submit another one."
+             + " The choice is entirely up to you."
+        },
+        VOTED_OUT: {
+            title: "Quest voted out",
+            message: 
+                "<p>"
+             + "Your quest was voted out on {date}, after a playtime of {time}"
+             + " (total playtime {total_time}, played {playtimes} times)."
+             + " The vote count ended at {yes_count} Yes vs. {no_count} No."
+             + "<p>"
+             + "Gikkman gave it a rating of '{rating}'. If you want to check"
+             + " out the entire review, please check out the " + QUEST_LOG + "."
+             + "<p>"
+             + "You now have the choice to either resubmit the same quest"
+             + " (in which case we will continue from where we left off)"
+             + " or you can chose to abandon the quest and submit another one."
+             + " The choice is entirely up to you."
+             + " Thanks for contributing to the Journey Project!"
+        },
+        VOTED_OUT_RESUBMITTED: {
+            title: "Quest voted out",
+            message: 
+                "<p>"
+             + "Your quest was voted out on {date}, after a playtime of {time}"
+             + " (total playtime {total_time}, played {playtimes} times)."
+             + " The vote count ended at {yes_count} Yes vs. {no_count} No."
+             + "<p>"
+             + "Gikkman gave it a rating of '{rating}'. If you want to check"
+             + " out the entire review, please check out the " + QUEST_LOG + "."
+             + "<p>"
+             + "Gikkman wanted to continue the quest some more, so he chose to "
+             + " auto-resubmit it. You can choose to leave it at that"
+             + " (in which case we will continue from where we left off)"
+             + " or you can choose to abandon the quest and submit another one. "
+             + " The choice is entirely up to you."
+             + " Thanks for contributing to the Journey Project!"
         }
     };
 };
