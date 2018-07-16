e941732c437e1ca1d38996adac16d62384858dd6
gikkman <ksr.jonasson@gmail.com>
2018-07-16 21:24:05 +0200

    minor fixes

--- modules/game_databases.js
+++ modules/game_databases.js
@@ -15,14 +15,14 @@ module.exports = function () {
 
     obj.createQuest = async (DB, title, system, goal) => {
         var sql = "INSERT INTO " + QUESTS
-            + " (title, system, goal, created, updated, state)"
+            + " (`title`, `system`, `goal`, `created`, `updated`, `state`)"
             + " VALUES (?,?,?,NOW(), NOW(), ?)";
         var data = await DB.queryAsync(sql, [title, system, goal, State.Q.submitted]);
         return data.insertId;
     };
 
     obj.deleteQuestHard = async (DB, quest) => {
-        var sql = "DELETE FROM " + QUESTS + " WHERE uid = ?";
+        var sql = "DELETE FROM " + QUESTS + " WHERE `uid` = ?";
         return await DB.queryAsync(sql, [quest.quest_id]);
     };
 
@@ -35,14 +35,14 @@ module.exports = function () {
         let allowedFields = limited ? questModifiableFieldsLimited : questModifiableFields;
 
         let sqlStart = "UPDATE " + QUESTS + " SET ";
-        let sqlArgs = "updated = NOW()";
+        let sqlArgs = "`updated` = NOW()";
         var args = [];
         for(let key in quest) {
             if(allowedFields.indexOf(key) === -1) continue;
-            sqlArgs += ", " +  key + " = ?";
+            sqlArgs += ", `" +  key + "` = ?";
             args.push(quest[key]);
         }
-        let sql = sqlStart + sqlArgs + " WHERE uid=?";
+        let sql = sqlStart + sqlArgs + " WHERE `uid`=?";
         args.push(quest.quest_id);
 
         let row = await DB.queryAsync(sql, args);
@@ -50,9 +50,9 @@ module.exports = function () {
     };
 
     obj.getQuestByID = async (DB, questID) => {
-        var sql = "SELECT uid AS quest_id, created, updated, title, "
-            + " system, goal, state, seconds_played, times_played "
-            + " FROM " + QUESTS + " WHERE uid = ?";
+        var sql = "SELECT `uid` AS quest_id, `created`, `updated`, `title`, "
+            + " `system`, `goal`, `state`, `seconds_played`, `times_played` "
+            + " FROM " + QUESTS + " WHERE `uid` = ?";
         let rows = await DB.queryAsync(sql, [questID]);
         return rows[0];
     };
@@ -70,19 +70,19 @@ module.exports = function () {
 
     obj.createSubmission = async (DB, questID, userID, comments) => {
         var sql = "INSERT INTO " + SUBMISSONS
-            + " (quest_id, user_id, comments, created, updated, state)"
+            + " (`quest_id`, `user_id`, `comments`, `created`, `updated`, `state`)"
             + " VALUES (?,?,?, NOW(), NOW(), ?)";
         let data = await DB.queryAsync(sql, [questID, userID, comments, State.S.submitted]);
         return data.insertId;
     };
 
     obj.deleteSubmission = async (DB, submission) => {
-        var sql = "UPDATE " + SUBMISSONS + " SET deleted = NOW() WHERE uid = ?";
+        var sql = "UPDATE " + SUBMISSONS + " SET `deleted` = NOW() WHERE `uid` = ?";
         return await DB.queryAsync(sql, [submission.submission_id]);
     };
 
     obj.deleteSubmissionHard = async (DB, submission) => {
-        var sql = "DELETE FROM " + SUBMISSONS + " WHERE uid = ?";
+        var sql = "DELETE FROM " + SUBMISSONS + " WHERE `uid` = ?";
         return await DB.queryAsync(sql, [submission.submission_id]);
     };
 
@@ -93,12 +93,12 @@ module.exports = function () {
         }
 
         let sqlStart = "UPDATE " + SUBMISSONS + " SET ";
-        let sqlArgs = "updated = NOW()";
+        let sqlArgs = "`updated` = NOW()";
         var args = [];
         for(let key in submission) {
             if(updateModifiableFields.indexOf(key) === -1) continue;
 
-            sqlArgs += ", " + key + " = ?";
+            sqlArgs += ", `" + key + "` = ?";
             let val = submission[key];
             if(val === 'NOW()'){
                 sqlArgs = sqlArgs.slice(0, -1) + val;
@@ -106,7 +106,7 @@ module.exports = function () {
                 args.push(val);
             }
         }
-        let sql = sqlStart + sqlArgs + " WHERE uid = ?";
+        let sql = sqlStart + sqlArgs + " WHERE `uid` = ?";
         args.push(submission.submission_id);
 
         let row = await DB.queryAsync(sql, args);
@@ -114,16 +114,16 @@ module.exports = function () {
     };
 
     obj.getSubmissionByUserID = async (DB, userID) => {
-        var sql = "SELECT s.uid AS submission_id, s.quest_id, s.user_id,"
-            + " s.created, s.updated, s.comments, s.state,"
-            + " s.start_date, s.seconds_played,"
-            + " IF(a.state IS NOT NULL, a.state, NULL) AS active_state"
-            + " FROM " + SUBMISSONS + " AS s"
+        var sql = "SELECT `s`.`uid` AS `submission_id`, `s`.`quest_id`, `s`.`user_id`,"
+            + " `s`.`created`, `s`.`updated`, `s`.`comments`, `s`.`state`,"
+            + " `s`.`start_date`, `s`.`seconds_played`,"
+            + " IF(`a`.`state` IS NOT NULL, `a`.`state`, NULL) AS `active_state`"
+            + " FROM " + SUBMISSONS + " AS `s`"
             + " LEFT JOIN"
-                + " (SELECT submission_id, state FROM " + ACTIVE
-                + " WHERE system = " + JOURNEY + ") AS a"
-                + " ON a.submission_id = s.uid"
-            + " WHERE s.user_id = ? AND s.deleted IS NULL";
+                + " (SELECT `submission_id`, `state` FROM " + ACTIVE
+                + " WHERE `system` = " + JOURNEY + ") AS `a`"
+                + " ON `a`.`submission_id` = `s`.`uid`"
+            + " WHERE `s`.`user_id` = ? AND `s`.`deleted` IS NULL";
         let rows = await DB.queryAsync(sql, [userID]);
 
         if (rows.lenght > 1)
@@ -133,16 +133,16 @@ module.exports = function () {
     };
 
     obj.getSubmissionBySubmissionID = async (DB, submissionID) => {
-        var sql = "SELECT s.uid AS submission_id, s.quest_id, s.user_id,"
-            + " s.created,  s.comments, s.state,"
-            + " s.start_date, s.seconds_played,"
-            + " IF(a.state IS NOT NULL, a.state, NULL) AS active_state"
-            + " FROM " + SUBMISSONS + " AS s"
+        var sql = "SELECT `s`.`uid` AS `submission_id`, `s`.`quest_id`, `s`.`user_id`,"
+            + " `s`.`created`,  `s`.`comments`, `s`.`state`,"
+            + " `s`.`start_date`, `s`.`seconds_played`,"
+            + " IF(`a`.`state` IS NOT NULL, `a`.`state`, NULL) AS `active_state`"
+            + " FROM " + SUBMISSONS + " AS `s`"
             + " LEFT JOIN"
-                + " (SELECT submission_id, state FROM " + ACTIVE
-                + " WHERE system = " + JOURNEY + ") AS a"
-                + " ON a.submission_id = s.uid"
-            + " WHERE s.uid = ? AND s.deleted IS NULL";
+                + " (SELECT `submission_id`, `state` FROM " + ACTIVE
+                + " WHERE `system` = " + JOURNEY + ") AS `a`"
+                + " ON `a`.`submission_id` = `s`.`uid`"
+            + " WHERE `s`.`uid` = ? AND `s`.`deleted` IS NULL";
         let rows = await DB.queryAsync(sql, [submissionID]);
 
         if (rows.lenght > 1)
@@ -155,49 +155,49 @@ module.exports = function () {
     //              ACTIVE
     //-----------------------------------------------------------
     obj.suspend = async(DB, submission) => {
-        let sql = "INSERT INTO " + ACTIVE + " (submission_id, system, state) "
+        let sql = "INSERT INTO " + ACTIVE + " (`submission_id`, `system`, `state`) "
             + " VALUES (?, " + JOURNEY + ", ?)";
         await DB.queryAsync(sql, [submission.submission_id, State.A.suspended]);
-    }
+    };
 
     obj.removeSuspended = async(DB, submission) => {
-        let sql = "DELETE FROM " + ACTIVE + " WHERE submission_id = ? AND state = ?";
+        let sql = "DELETE FROM " + ACTIVE + " WHERE `submission_id` = ? AND `state` = ?";
         await DB.queryAsync(sql, [submission.submission_id, State.A.suspended]);
-    }
+    };
 
     obj.advanceActives = async (DB) => {
         var sqlDelete = "DELETE FROM " + ACTIVE
-            + " WHERE system = " + JOURNEY + " AND state = ?";
+            + " WHERE `system` = " + JOURNEY + " AND `state` = ?";
         var sqlUpdate = "UPDATE " + ACTIVE + " SET "
-            + " state = ? WHERE system = " + JOURNEY + " AND state = ?";
+            + " `state` = ? WHERE `system` = " + JOURNEY + " AND `state` = ?";
         let deleteRow = await DB.queryAsync(sqlDelete, [State.A.current]);
         let updateRow = await DB.queryAsync(sqlUpdate, [State.A.current, State.A.next]);
         return { deleted: deleteRow.affectedRows, updated: updateRow.affectedRows};
     };
 
     obj.setNextActive = async (DB, submission) => {
-        var sql = "INSERT INTO " + ACTIVE + " (submission_id, system, state) "
+        var sql = "INSERT INTO " + ACTIVE + " (`submission_id`, `system`, `state`) "
             + " VALUES (?," + JOURNEY + ",?)";
         let row = await DB.queryAsync(sql, [submission.submission_id, State.A.next]);
         return row.affectedRows;
     };
 
     obj.getNextActive = async (DB) => {
-        var sql = "SELECT a.*, s.* "
+        var sql = "SELECT `a`.*, `s`.* "
                 + " FROM " + ACTIVE + " AS a"
-                    + " LEFT JOIN game_submission AS s"
-                    + " ON s.uid = a.submission_id"
-                + " WHERE a.system = " + JOURNEY + " AND a.state = ?";
+                    + " LEFT JOIN `game_submission` AS `s`"
+                    + " ON `s`.`uid` = `a`.`submission_id`"
+                + " WHERE `a`.`system` = " + JOURNEY + " AND `a`.`state` = ?";
         let rows = await DB.queryAsync(sql, [State.A.next]);
         return rows[0];
     };
 
     obj.getCurrentActive = async (DB) => {
-        var sql = "SELECT a.*, s.* "
-                + " FROM " + ACTIVE + " AS a"
-                    + " LEFT JOIN game_submission AS s"
-                    + " ON s.uid = a.submission_id"
-                + " WHERE a.system = " + JOURNEY + " AND a.state = ?";
+        var sql = "SELECT `a`.*, `s`.* "
+                + " FROM " + ACTIVE + " AS `a`"
+                    + " LEFT JOIN `game_submission` AS `s`"
+                    + " ON `s`.`uid` = `a`.`submission_id`"
+                + " WHERE `a`.`system` = " + JOURNEY + " AND `a`.`state` = ?";
         let rows = await DB.queryAsync(sql, [State.A.current]);
         return rows[0];
     };
