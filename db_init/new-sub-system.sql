347f71274c44d77c19a265377550cd7dc4eb207e
gikkman <ksr.jonasson@gmail.com>
2018-08-11 02:03:58 +0200

    more db changes

--- db_init/new-sub-system.sql
+++ db_init/new-sub-system.sql
@@ -6,6 +6,9 @@ ALTER TABLE `journey`.`game_active`
 ADD COLUMN `index` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `state`,
 ADD COLUMN `subindex` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `index`;
 
+ALTER TABLE `journey`.`game_active` 
+DROP INDEX `game_active_unique` ;
+
 UPDATE journey.game_active SET `index` = (SELECT MAX(`index`) FROM gamesplayed) + 1 WHERE `system` = 'journey' AND `state` = 'current';
 UPDATE journey.game_active SET `index` = (SELECT MAX(`index`) FROM gamesplayed) + 2 WHERE `system` = 'journey' AND `state` = 'next';
 
@@ -13,8 +16,13 @@ UPDATE journey.game_active SET `index` = (SELECT MAX(`index`) FROM gamesplayed) 
 
 ALTER TABLE `journey`.`gamesplayed` 
 ADD COLUMN `subindex` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `sub_index`;
+ADD COLUMN `vote_timer` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `subindex`;
 
 UPDATE gamesplayed SET subindex = 1 WHERE sub_index = "B" AND `uid` > 1;
 
 ALTER TABLE `journey`.`gamesplayed` 
 DROP COLUMN `sub_index`;
+
+
+
+INSERT INTO `journey`.`config` (`key`, `value`) VALUES ('vote_time_init', '5400');
