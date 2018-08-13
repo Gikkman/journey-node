346c2b15fcd6fc1a79e51be0298e1a2cfb330206
gikkman <ksr.jonasson@gmail.com>
2018-08-13 23:07:11 +0200

    update the database query to forward the db

--- db_init/new-sub-system.sql
+++ db_init/new-sub-system.sql
@@ -2,6 +2,11 @@
  * Author:  Gikkman
  * Created: Jul 25, 2018
  */
+ALTER TABLE `journey`.`game_submission`
+ADD COLUMN `dn_override` VARCHAR(45) NULL AFTER `end_date`;
+
+
+
 ALTER TABLE `journey`.`game_active` 
 ADD COLUMN `index` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `state`,
 ADD COLUMN `subindex` INT UNSIGNED NOT NULL DEFAULT 0 AFTER `index`;
